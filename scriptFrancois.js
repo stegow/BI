f7c70e95f75b9bc8681bb34916f554ad3c3bae51
Flaele <francois.jones@outlook.com>
2014-12-06 17:25:18 +0100

    Update scriptFrancois.js

--- scriptFrancois.js
+++ scriptFrancois.js
@@ -3,8 +3,8 @@ function drawPie(id, column) {
 			  title: column,
 			  vAxis: {title: X,  titleTextStyle: {color: 'red'}},
 			  hAxis: {title: Y,  titleTextStyle: {color: 'red'}},
-			  width: $(id).width();,
-			  height: $(id).height();
+			  width: $(id).width(),
+			  height: $(id).height()
 			};
 			myData = JSONextractCount(column,activeData);
 			chart = new google.visualization.PieChart(document.getElementById(id));
