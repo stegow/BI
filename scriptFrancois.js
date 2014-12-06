ae4ee9adb074903db624c48408cb6a6b88302d45
Flaele <francois.jones@outlook.com>
2014-12-06 16:57:23 +0100

    drawPie()

--- scriptFrancois.js
+++ scriptFrancois.js
@@ -1,8 +1,18 @@
-var typeChart;
-var X;
+var options;
+var myData;
+var data;
+var chart;
 
-$(document).ready(function () {
-  typeChart = "pie";
-  X = "node";
-  drawChart("pie_1");
-});
+function drawPie(var id) {
+  			options = {
+			  title: X + ' vs. ' + Y,
+			  vAxis: {title: X,  titleTextStyle: {color: 'red'}},
+			  hAxis: {title: Y,  titleTextStyle: {color: 'red'}},
+			  width: 800,
+			  height: 480
+			};
+			myData = JSONextractCount("node",activeData);
+			chart = new google.visualization.PieChart(document.getElementById(id));
+			data = google.visualization.arrayToDataTable(myDataPie);
+			chart.draw(data, options);
+}
